5d20b7736ab24147302479685b4f34b880878142
Liu Xia <lxconan@gmail.com>
2019-02-13 10:31:33 +0800

    [liuxia] feat: initailize final sample application.

---
+++ 99-beautiful/src/WebApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+
+namespace WebApp
+{
+    static class Program
+    {
+        public static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>().Build().Run();
+        }
+    }
+}
---
+++ 99-beautiful/src/WebApp/Startup.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace WebApp
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // In order to create mvc related instances, we should add these definitions to
+            // DI services. This is kind of a shared infrastructure.
+            services.AddMvc();
+        }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            // In order to let mvc come into play. We should add mvc middleware to the pipeline.
+            app.UseMvc();
+        }
+    }
+}
