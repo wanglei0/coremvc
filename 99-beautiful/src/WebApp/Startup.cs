63d3a3763f9f1c06b436a2d8733843b67a8dc088
Liu Xia <lxconan@gmail.com>
2019-02-15 23:07:09 +0800

    [liuxia] feat: add singleton HTTP client to 3rd party services.

--- 99-beautiful/src/WebApp/Startup.cs
+++ 99-beautiful/src/WebApp/Startup.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.DependencyInjection;
@@ -26,6 +28,23 @@ namespace WebApp
             // It is not good to create a module called "Infrastructure Module" because the module
             // should represent business rather than a layer.
             services.AddMvc();
+            services.AddSingleton(_ =>
+            {
+                var handler = new HttpClientHandler
+                {
+                    // Add your own SSL validation rule here to ensure cert can be trusted. Here
+                    // we just return true indicating that we will simply accept the cert.
+                    ServerCertificateCustomValidationCallback =
+                        (message, cert, chain, error) => true,
+                    
+                    // Normally server side http client will treat redirection as a failure, or
+                    // will redirect manually. You can change this settings on demand.
+                    AllowAutoRedirect = false
+                };
+                
+                // Modify timeout value. TODO: expose it via configuration
+                return new HttpClient(handler) {Timeout = TimeSpan.FromSeconds(20)};
+            });
         }
 
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
