f68000230afb12a4b2f9c7d54a1e547d427f3d82
Liu Xia <lxconan@gmail.com>
2019-03-08 10:53:52 +0800

    [liuxia] delete: remove 99-1. We have found a better way.

--- 99-1-beautiful/src/WebApp.Deployment/IWebHostConfiguratorForEnvironment.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Hosting;
-
-namespace WebApp.Deployment
-{
-    public interface IWebHostConfiguratorForEnvironment
-    {
-        void Configure(IWebHostBuilder builder);
-    }
-}
