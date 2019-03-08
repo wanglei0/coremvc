d3f744a652f426a417e179abfb580e73b5ae0e68
Liu Xia <lxconan@gmail.com>
2019-03-08 10:54:23 +0800

    [liuxia] delete: remove 99. We have found a better way.

--- 99-beautiful/src/WebModule.HealthCheck/IHealthCheckConfig.cs
+++
@@ -1,7 +0,0 @@
-namespace WebModule.HealthCheck
-{
-    class HealthCheckConfig
-    {
-        public string ConnectionString { get; set; }
-    }
-}
--- 99-beautiful/test/WebApp.Test/Utils/TypeExtension.cs
+++
@@ -1,7 +0,0 @@
-namespace WebApp.Test.Utils
-{
-    static class TypeExtension
-    {
-        public static T[] AsArrayType<T>(this T instance) { return null; }
-    }
-}
